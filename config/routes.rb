83cd5fcf5bf99a625f5f0357fd71701d3561157b
George Patterson <gp00482@surrey.ac.uk>
2021-04-06 11:36:23 +0100

    Added some routes and partials

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'profile/profile'
   get 'profile/edit'
   get 'profile/change_password'
-  get 'calculator/calculator'
+  get '/calculator', to: 'calculator#calculator'
   get 'home/home'
   get 'home/about'
   root 'home#home'
