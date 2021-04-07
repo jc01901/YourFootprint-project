c102d2d42c2c9e425bccc0221de3f854e0837293
Thamiem Noori <tn00347@surrey.ac.uk>
2021-04-07 18:14:10 +0100

    adding contact/about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,24 @@
 class HomeController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def home
   end
 
   def about
   end
+
+  def request_contact
+    name = params[:name]
+    email = params[:email]
+    telephone = params[:telephone] 
+    message = params[:message]
+    
+    if email.blank?
+      flash.alert = "You have to enter a valid email"
+      redirect_to request.referer
+    else
+      flash.notice = "Your message has been sent"
+      redirect_to root_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'calculator/calculator'
   get 'home/home'
   get 'home/about'
+  get 'about', to: 'home#about'
+  post 'request_contact', to: 'home#request_contact'
   root 'home#home'
 end
