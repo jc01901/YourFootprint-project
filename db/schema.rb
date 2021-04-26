1055887b78caa4699e040ae712ddec05921a32f0
Ralph Gonzales <rg00663@surrey.ac.uk>
2021-04-26 17:36:51 +0100

    bootsnap 1.7.3 -> 1.7.4

--- db/schema.rb
+++ db/schema.rb
@@ -31,11 +31,6 @@ ActiveRecord::Schema.define(version: 2021_04_07_172237) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "firstname"
-    t.string "surname"
-    t.date "dob"
-    t.string "post_code"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
