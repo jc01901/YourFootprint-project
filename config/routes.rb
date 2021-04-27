df4d02b35f58b448bfdad3dcbcdc617e1abce24f
James Churcher <jc01901@surrey.ac.uk>
2021-04-27 19:06:36 +0100

    Cleaned routes, fixed style warning on info view

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,22 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  get 'weather/weather'
-  get 'info/info'
-  get '/profile', to: 'profile#profile'
-  get '/leaderboard', to: 'leaderboard#leaderboard'
-  get 'profile/edit'
-  get 'profile/change_password'
-  get '/calculator', to: 'calculator#calculator'
-  get 'home/home'
-  get 'home/about'
-  get 'about', to: 'home#about'
-  post 'request_contact', to: 'home#request_contact'
-  get 'information', to: 'info#info'
-  get 'weather', to: 'weather#weather'
+  # get 'weather/weather'
+  # get 'info/info'
+  # get 'home/home'
+  # get 'home/about'
 
   root 'home#home'
+
+  get 'information', to: 'info#info'
+  get 'calculator', to: 'calculator#calculator'
+  get 'weather', to: 'weather#weather'
+  get 'leaderboard', to: 'leaderboard#leaderboard'
+  get 'about', to: 'home#about'
+  post 'request_contact', to: 'home#request_contact'
+  
+  get 'profile', to: 'profile#profile'
+  get 'profile/edit'
+  get 'profile/change_password'
 
 end
