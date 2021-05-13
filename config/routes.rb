10690a09814bea9ecff518c645b814feaab8b24a
James Churcher <jc01901@surrey.ac.uk>
2021-05-13 14:34:59 +0100

    Remove the weather page

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   get 'information', to: 'info#info'
   get 'calculator', to: 'calculator#calculator'
-  get 'weather', to: 'weather#weather'
+  # get 'weather', to: 'weather#weather'
   get 'leaderboard', to: 'leaderboard#leaderboard'
   get 'about', to: 'home#about'
   post 'request_contact', to: 'home#request_contact'
