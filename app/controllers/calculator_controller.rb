210a72008fd8b5a7d8c8192ffa34ecd7a028ad74
Ross Treadway <rt00492@surrey.ac.uk>
2021-05-16 16:28:16 +0100

    Fixing entrypoint.sh and adding extra validations on calculator forms

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -12,8 +12,8 @@ class CalculatorController < ApplicationController
     else
     
       # collect user inputs and give them variables
-      power = params["calculator"]["power"].to_f()
-      distance = params["calculator"]["distance"].to_f()
+      power = params["calculator"]["power"]
+      distance = params["calculator"]["distance"]
       distance_type = params["calculator"]["distanceRadios"]
       fuel_type = params["calculator"]["fuelRadios"]
       if distance_type.blank? || fuel_type.blank?
@@ -24,20 +24,37 @@ class CalculatorController < ApplicationController
           @errors.push("You must select a fuel unit")
         end
         render 'calculator'
-      else
-        converted_inputs = helpers.co2e_conversion(power, distance, distance_type, fuel_type)
-        
+      else 
+        if power == "" && distance == ""
+          @errors.push("You must enter at least one of either power or distance")
+          render 'calculator'
+        else
+          if power =~ /[a-zA-Z]/ || distance =~ /[a-zA-Z]/
+            if power =~ /[a-zA-Z]/
+              @errors.push("You must enter a number for power")
+            end
+            if distance =~ /[a-zA-Z]/
+              @errors.push("You must enter a number for distance")
+            end
+            render 'calculator'          
+          else
+            power = power.to_f()
+            distance = distance.to_f()
+            converted_inputs = helpers.co2e_conversion(power, distance, distance_type, fuel_type)
+            
 
-        # create User_data database record
-        UserData.create(user_id: current_user.id, date: DateTime.current.midnight, petrol: converted_inputs["petrol_co2e"].ceil + converted_inputs["diesel_co2e"].ceil, electricity_usage: converted_inputs["power_co2e"].ceil, user_points: converted_inputs["user_points"])
-        lifetime_points = current_user.lifetime_points
-        # update user lifetime_points 
-        User.find(current_user.id).update_attribute(:lifetime_points, (lifetime_points + converted_inputs["user_points"]))
-        @success.push("Successfully submitted data!")
-        # update view
-        render 'calculator'
-        # send user back to homepage (not final)
-      # redirect_to root_path
+            # create User_data database record
+            UserData.create(user_id: current_user.id, date: DateTime.current.midnight, petrol: converted_inputs["petrol_co2e"].ceil + converted_inputs["diesel_co2e"].ceil, electricity_usage: converted_inputs["power_co2e"].ceil, user_points: converted_inputs["user_points"])
+            lifetime_points = current_user.lifetime_points
+            # update user lifetime_points 
+            User.find(current_user.id).update_attribute(:lifetime_points, (lifetime_points + converted_inputs["user_points"]))
+            @success.push("Successfully submitted data!")
+            # update view
+            render 'calculator'
+            # send user back to homepage (not final)
+          # redirect_to root_path
+          end
+        end
       end
     end
   end
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Remove a potentally pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /tmp/pids/server.pid
 
 # Sleep for 14 seconds to allow mysql db to setup and connect
 # sleep 14
