6c04a6234dbfd900ddf91b65771988adfd8b3ee8
mb01761 <mb01761@surrey.ac.uk>
2021-05-17 02:18:02 -0700

    Testing and validation for database

--- app/models/user_data.rb
+++ app/models/user_data.rb
@@ -1,3 +1,5 @@
 class UserData < ApplicationRecord
   belongs_to :user
+  validates_presence_of :electricity_usage, :petrol, :user_points
+
 end
--- test/models/user_data_test.rb
+++ test/models/user_data_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserDataTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should not save without electricity_usage' do
+    record = UserData.create({
+      :petrol => 350,
+      :user_points => 25,
+      })
+    record.save
+    refute record.valid?
+  end
+
+  test 'should not save without petrol' do
+    record = UserData.create({
+      :electricity_usage => 350,
+      :user_points => 25,
+      })
+    record.save
+    refute record.valid?
+  end
+
+  test 'should not save without user_points' do
+    record = UserData.create({
+      :petrol => 350,
+      :electricity_usage => 25,
+      })
+    record.save
+    refute record.valid?
+  end
 end
